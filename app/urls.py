8d2ef2a57f313df2a31fa7661a4796fc6c89cbb8
ashu3110 <avsingh000.789@gmail.com>
2022-11-02 12:59:36 +0530

    college project 3
    This is my college project and this project base on online shoping system

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path("", views.index, name='apps'),
+    path('signup', views.handleSignUp, name='handleSignUp'),
+    path('login', views.handeLogin, name='handleLogin'),
+    path('logout', views.handelLogout, name='handleLogout'),
+    path("login", views.login, name='login'),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("checkout/", views.checkout, name="Checkout"),
+    path("products/<int:myid>", views.productView, name="productView"),
+
+]
